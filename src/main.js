4d70749bb49c74f5da5ad6faa90f5ff952b87b58
Lucas Maieski <lucasgrigol@gmail.com>
2022-10-21 20:25:55 -0300

    finish form validation

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import "./css/index.css";
 import IMask from "imask";
 
-
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
-
-console.log(ccBgColor01);
-
+const inputs = document.querySelectorAll("input")
 
 function setCardType(type) {
   const colors = {
@@ -19,10 +16,8 @@ function setCardType(type) {
   ccBgColor02.setAttribute("fill", colors[type][1])
   ccLogo.setAttribute("src", `public/cc-${type}.svg`)
 }
-
 setCardType("default");
 
-// security code
 const securityCode = document.querySelector('#security-code');
 const securityCodePattern = {
   mask: "0000"
@@ -46,6 +41,7 @@ const expirationDatePattern = {
   }
 }
 const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
 
 const cardNumber = document.querySelector('#card-number')
 const cardNumberPattern = {
@@ -76,27 +72,50 @@ const cardNumberPattern = {
 }
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
 
-const addButton = document.querySelector("#add-card")
-addButton.addEventListener("click", () => {
-  alert("Cartão adicionado")
-})
-
-document.querySelector("form").addEventListener("submit", (event) => {
-  event.preventDefault()
-})
-
 const cardHolder = document.querySelector("#card-holder")
-cardHolder.addEventListener("input", () => {
-  const ccHolder = document.querySelector(".cc-holder .value")
-  ccHolder.innerText = cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+
+function maskNome(value) {
+  console.log(value)
+  return value
+    .replace(/\d/g, "")
+    .replace(/\s{2,}/, " ")
+    .replace(/[^a-z\s]/, "")
+    .replace(/^\s/, "")
+    .replace(/^(\D{1,2})\s/, "$1")
+    .replace(/(\s\D{1})\s/, "$1")
+}
+
+cardHolder.addEventListener("input", (e) => {
+  e.target.value = maskNome(e.target.value)
+  updateCardHolder(e.target.value)
 })
+function updateCardHolder(holder) {
+  const ccHolder = document.querySelector(".cc-holder .value")
+  if (holder.length === 0) {
+    ccHolder.innerText = "FULANO DA SILVA"
+    borderCheck(cardHolder, false)
+  }else {
+    ccHolder.innerText = holder
+    if (cardHolder.value.length > 2) {
+      borderCheck(cardHolder, true)
+    } else { cardHolder.classList.remove("check") }
+  }
+}
 
 securityCodeMasked.on("accept", () => {
   updateSecurityCode(securityCodeMasked.value)
 })
 function updateSecurityCode(code) {
   const ccSecurity = document.querySelector(".cc-security .value")
-  ccSecurity.innerText = code.length === 0 ? "123" : code
+  if(code.length === 0) {
+    ccSecurity.innerText = "123"
+    borderCheck(securityCode, false)
+  } else {
+    ccSecurity.innerText = code
+    if(code.length === 4) {
+      borderCheck(securityCode, true)
+    } else { securityCode.classList.remove("check") }
+  }
 }
 
 cardNumberMasked.on("accept", () => {
@@ -106,7 +125,15 @@ cardNumberMasked.on("accept", () => {
 })
 function updateCardNumber(number) {
   const ccNumber = document.querySelector(".cc-number")
-  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+  if(number.length === 0){
+    ccNumber.innerText = "1234 5678 9012 3456"
+    borderCheck(cardNumber, false)
+  } else {
+    ccNumber.innerText = number
+    if(number.length === 19) {
+      borderCheck(cardNumber, true)
+    }else { cardNumber.classList.remove("check") }
+  }
 }
 
 expirationDateMasked.on("accept", () => {
@@ -115,4 +142,57 @@ expirationDateMasked.on("accept", () => {
 function updateExpirationDate(date) {
   const ccExpiration = document.querySelector(".cc-expiration .value")
   ccExpiration.innerText = date.length === 0 ? "02/32" : date
+
+  if (date.length === 0) {
+    ccExpiration.innerText = "02/32"
+    borderCheck(expirationDate, false)
+  } else {
+    ccExpiration.innerText = date
+    if (date.length === 5) {
+      borderCheck(expirationDate, true)
+    } else { expirationDate.classList.remove("check") }
+  }
+}
+
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", (e) => {
+  let resultado = checkInputs()
+  if (resultado != 0) {
+    e.preventDefault()
+    console.log(cardNumber.value.length)
+    alert("Preencha corretamente os campos sinalizados em vermelho!")
+  } else {
+    document.querySelector("form").reset()
+    alert("Cartão adicionado com sucesso")
+    inputs.forEach(function (item) {
+      item.classList.remove("check")
+    })
+  }
+})
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault()
+})
+
+function checkInputs() {
+  if (cardNumber.value.length === 19 && securityCode.value.length === 4 && cardHolder.value.length > 2 && expirationDate.value.length === 5) {
+    return 0
+  } else {
+    inputs.forEach(function (item) {
+      if (!item.classList.contains("check")){
+        item.classList.add("nocheck")
+      }
+    })
+    return 1
+  }
+}
+
+function borderCheck(object, i) {
+  if (i) {
+    object.classList.add("check")
+    object.classList.remove("nocheck")
+  } else {
+    object.classList.add("nocheck")
+    object.classList.remove("check")
+  }
 }
